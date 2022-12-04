9224765a6cb213a236206715055c9bbbb817d724
Rodolfo Yap <yap19jr@gmail.com>
2022-12-04 20:13:53 +0800

    Update webpack bundling config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname,"app")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ["style-loader","css-loader"]
+            }
+        ]
+    }
+
+}
