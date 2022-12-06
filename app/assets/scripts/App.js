ec5ec21380755cf356e6b41526401c238f39d349
Rodolfo Yap <yap19jr@gmail.com>
2022-12-06 20:09:32 +0800

    change some files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,6 @@
 import '../styles/styles.css'
+import '../styles/styles.css'
+
+if (module.hot) {
+  module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require('path')
 module.exports = {
-    entry: "./app/assets/scripts/App.js",
-    output: {
-        filename: "bundled.js",
-        path: path.resolve(__dirname,"app")
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css/i,
-                use: ["style-loader","css-loader"]
-            }
-        ]
-    }
-
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 }
